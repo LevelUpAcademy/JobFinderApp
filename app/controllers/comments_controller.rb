d04b9c66d36f6f0e28a641c453caac50ac8d9fa7
Jonatan Kapps <dev@levelupacademy.com.br>
2017-09-23 09:45:18 -0300

    S principle

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,8 @@ class CommentsController < ApplicationController
     job_id = params.require(:comment)[:job]
     text = params.require(:comment)[:message]
 
-    Comment.new({Job_id: job_id, message: text}).add_comment
+    new_comment = Comment.new({Job_id: job_id, message: text})
+    new_comment.add_comment
 
     redirect_to job_path(job_id)
   end
